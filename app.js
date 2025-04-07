330f2ec050c79e1891f43b96f76fbf5cd07b1909
beatriz <128046453+abeazist@users.noreply.github.com>
2025-04-07 11:39:01 -0300

    update app.js

--- app.js
+++ app.js
@@ -13,6 +13,20 @@ app.get('/homepage', function(req,res,next){
     res.send("bea linda!")
 });
 
+//PASAGEM POR PARAMETRO USANDO QUERY STRING
+//localhost:8086/ola2?nome=Beatriz&sobrenome=Oliveira
+app.get('/ola2', function (req,res){
+    const {nome,sobrenome} = req.query
+    res.send(`Bem vindo, ${nome} ${sobrenome}`)
+});
+
+app.get('/ola/:nome/:sobrenome', function (req,res){
+    res.send(`Bem vindo, ${req.params.nome} ${req.params.sobrenome}`)
+})
+
+
+
+
 app.listen(port, () => {
     console.log(`servidor rodando na porta ${port}...`)
 });
